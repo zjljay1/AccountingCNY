943a8ad9334f9b9c85f6353f6201db83443ab66a
Jay <1990843516@qq.com>
2022-08-10 23:53:55 +0800

    :truck:vue文件移动

--- accountingCNY_vue/src/App.vue
+++ accountingCNY_vue/src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+
+console.log(123);
+
+console.log(456);
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
