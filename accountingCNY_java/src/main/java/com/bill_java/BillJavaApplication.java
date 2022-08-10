404cf215a27e4353bd181505e7e886fb4139d3d1
Jay <1990843516@qq.com>
2022-08-11 00:01:48 +0800

    :construction:java文件创建

---
+++ accountingCNY_java/src/main/java/com/bill_java/BillJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bill_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillJavaApplication.class, args);
+	}
+
+}
---
+++ accountingCNY_java/src/test/java/com/bill_java/BillJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
